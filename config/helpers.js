0485ea58adb62048287d1ba46dbdffbe4658ac77
Miszo Radomski <miszo@icloud.com>
2017-07-02 16:57:29 +0200

    added AUTH_URL in helpers

--- config/helpers.js
+++ config/helpers.js
@@ -1,4 +1,5 @@
 export const CLIENT_ID = process.env.CLIENT_ID || 'CLIENT_ID';
 export const CLIENT_SECRET = process.env.CLIENT_SECRET || 'CLIENT_SECRET';
 export const TOKEN_NAME = process.env.TOKEN_NAME || 'TOKEN_NAME';
-export const API_URL = 'https://foursquare.com/oauth2';
+export const AUTH_URL = 'https://foursquare.com/oauth2/authenticate'
+export const API_URL = 'https://api.foursquare.com/v2';
